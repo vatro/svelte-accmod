d7c1a6d00a61a1101733930b932f903bf9b18a48
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2021-10-23 22:45:50 +0200

    Fix 'test\runtime\samples\prop-accessors'

--- test/runtime/samples/prop-accessors/_config.js
+++ test/runtime/samples/prop-accessors/_config.js
@@ -1,9 +1,14 @@
 export default {
-	accessors: false,
+	// in 'accmod' 'accessors' are always set to 'true'
+	//accessors: false, // -> original test
 	test({ assert, component, flush }) {
 		assert.equal(component.foo1, 42);
 		assert.equal(component.foo2(), 42);
-		assert.equal(component.bar, undefined);
+
+		// 'component.bar' should be undefined (original test) only if 'accessors:false' (original test),
+		// in 'accmod' with 'accessors' always set to 'true', 'component.bar' should be '42'
+		assert.equal(component.bar, 42); 
+
 		component.foo1 = null;
 		flush();
 		component.foo2 = null;
