e24792428fc7861553d277b0ee33e246d7f11683
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2021-10-29 01:26:10 +0200

    Add runtime test 'accmod-reactive-statements-objects'

---
+++ test/runtime/samples/accmod-reactive-statements-objects/main.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { afterUpdate } from "svelte";
+	import Child from "./Child.svelte";
+
+	export let child;
+	let updates = 0;
+
+	afterUpdate(() => {
+		updates++;
+	});
+
+	export function set_new_key_in_child_foo_obj(key, key_value) {
+		child.foo[key] = key_value;
+	}
+
+	export function change_key_in_child_foo_obj(key, key_value) {
+		child.foo[key] = key_value;
+	}
+
+	export function set_new_key_in_child_foo_obj_benefit(key, key_value) {
+		// REACTIVE!
+		// this doesn't work that simple in 'unmodified' SVELTE , see https://svelte.dev/tutorial/updating-arrays-and-objects
+		const foo = child.foo;
+		foo[key] = key_value;
+	}
+
+	export function change_key_in_child_foo_obj_benefit(key, key_value) {
+		// REACTIVE!
+		// this doesn't work that simple in 'unmodified' SVELTE , see https://svelte.dev/tutorial/updating-arrays-and-objects
+		const foo = child.foo;
+		foo[key] = key_value;
+	}
+</script>
+
+main updates: {updates}
+<Child bind:this={child} />
