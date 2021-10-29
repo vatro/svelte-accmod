227636f2dba229cd2a758c1c088b2d023471b2e7
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2021-10-30 00:19:09 +0200

    Add runtime test 'accmod-reactive-statements-objects--slotted-deep'

---
+++ test/runtime/samples/accmod-reactive-statements-objects--slotted-deep/Parent.svelte
@@ -0,0 +1,13 @@
+<script>
+import { afterUpdate } from "svelte";
+let updates = 0
+export let depth
+
+afterUpdate(() => {
+		updates++;
+});
+
+</script>
+
+parent-{depth} updates: {updates}
+<slot />
