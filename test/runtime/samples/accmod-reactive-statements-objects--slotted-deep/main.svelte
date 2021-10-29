7a97205d47a7c5b400ced502a8c3f7b2fd1e3d55
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2021-10-30 00:19:09 +0200

    Add runtime test 'accmod-reactive-statements-objects--slotted-deep'

---
+++ test/runtime/samples/accmod-reactive-statements-objects--slotted-deep/main.svelte
@@ -0,0 +1,47 @@
+<script>
+	import { afterUpdate } from "svelte";
+	import Child from "./Child.svelte";
+	import Parent from "./Parent.svelte";
+
+	export let child;
+	let updates = 0;
+
+	afterUpdate(() => {
+		updates++;
+	});
+
+	export function set_new_key_in_child_foo_obj(key, key_value) {
+		child.foo[key] = key_value;
+	}
+
+	export function change_key_in_child_foo_obj(key, key_value) {
+		child.foo[key] = key_value;
+	}
+
+	export function set_new_key_in_child_foo_obj_benefit(key, key_value) {
+		// REACTIVE!
+		// this doesn't work that simple in 'unmodified' SVELTE , see https://svelte.dev/tutorial/updating-arrays-and-objects
+		const foo = child.foo;
+		foo[key] = key_value;
+	}
+
+	export function change_key_in_child_foo_obj_benefit(key, key_value) {
+		// REACTIVE!
+		// this doesn't work that simple in 'unmodified' SVELTE , see https://svelte.dev/tutorial/updating-arrays-and-objects
+		const foo = child.foo;
+		foo[key] = key_value;
+	}
+</script>
+
+main updates: {updates}
+<Parent depth={0}>
+	<Parent depth={1}>
+		<Parent depth={2}>
+			<Parent depth={3}>
+				<Parent depth={4}>
+					<Child bind:this={child} />
+				</Parent>
+			</Parent>
+		</Parent>
+	</Parent>
+</Parent>
