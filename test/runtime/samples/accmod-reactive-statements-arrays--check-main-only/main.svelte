922db46ce3f23cd51c3a36b0b9ba759035af3083
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2021-11-03 01:19:56 +0100

    [tests] Add / rename 'accmod-reactive-statements-arrays' tests

--- test/runtime/samples/accmod-reactive-statements-arrays--check-main-only/main.svelte
+++ test/runtime/samples/accmod-reactive-statements-arrays--check-main-only/main.svelte
@@ -10,23 +10,17 @@
 	});
 
 	// see https://svelte.dev/tutorial/updating-arrays-and-objects
-	// in 'svelte-accmod' using array methods like push and splice won't automatically cause updates
+	// in 'svelte-accmod' using array methods like push and splice won't be reactive
 	// just like in unmodified Svelte, but we're able to manipulate arrays using simpler syntax
 	// not having to copy / clone / spread existing arrays.
 
 	export function add_item_to_child_foo_at_specific_index(i, value) {
-		// (!!!) this isn't reactive / doesn't work, just like in unmodified Svelte
-		//child.foo.push(value);
-
 		// assigning a value to specific element / index of an array
 		// reactive -> this will trigger foo-reactive-statements and a child update!
 		child.foo[i] = value;
 	}
 
 	export function change_item_in_child_foo_at_specific_index(i, value) {
-		// (!!!) this isn't reactive / doesn't work, just like in unmodified Svelte
-		//child.foo.push(value);
-
 		// assigning a value to specific element / index of an array
 		// reactive -> this will trigger foo-reactive-statements and a child update!
 		child.foo[i] = value;
@@ -37,18 +31,12 @@
 	}
 
 	export function add_item_to_child_foo_at_end(value) {
-		// (!!!) this isn't reactive / doesn't work, just like in unmodified Svelte
-		//child.foo.push(value);
-
 		// assigning a value to the last element of array
 		// reactive -> this will trigger foo-reactive-statements and a child update!
 		child.foo[child.foo.length] = value;
 	}
 
 	export function recreate_child_foo_clone_spread_new_array(new_array) {
-		// (!!!) this isn't reactive / doesn't work, just like in unmodified Svelte
-		//child.foo.push(value);
-
 		// assigning a value to the last element of array
 		// reactive -> this will trigger foo-reactive-statements and a child update!
 		child.foo = [...new_array];
