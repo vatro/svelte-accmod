8545403f64a16091647890be346893bfb50a1edc
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2021-11-03 01:19:56 +0100

    [tests] Add / rename 'accmod-reactive-statements-arrays' tests

--- test/runtime/samples/accmod-reactive-statements-arrays--check-main-only/_config.js
+++ test/runtime/samples/accmod-reactive-statements-arrays--check-main-only/_config.js
@@ -1,4 +1,7 @@
+import { tick } from 'svelte';
+
 export default {
+	skip: true,
 
 	async test({ assert, component, target, flush, compileOptions }) {
 
@@ -7,14 +10,8 @@ export default {
 
 		assert.htmlEqual(target.innerHTML, `
 			main updates: 1
-			child updates: 1
-			child foo: , rs_foo: 1, foo.length: 0
-			child rs_foo_0: 0
-			child rs_foo_1: 0
-			child rs_foo_2: 0
-			child rs_foo_3: 0
-			child rs_foo_4: 0
 		`);
+
 
 		// IMPORTANT  'accmod' Remark:
 		// Reactive statements bound to computed array item refrences, e.g. `arr[0] = 'a'; $: arr[0] ? ...`
@@ -23,63 +20,40 @@ export default {
 		// also NO component update will be triggered, which is correct / wanted behavior!
 
 		// SVELTE 'unmodified': bad -> would trigger main-update +1 (wrong / unwanted) only, nothing else!
+		// TODO  if we set 'useAccMod' and 'accessorsAsync' to 'false' and test, we don't get the (unwanted) additional 'main' update,
+		// this is different than in REPL, WHY?
 		component.add_item_to_child_foo_at_specific_index(0, 'a');
-		compileOptions.accessorsAsync ? flush() : null;
+		await tick();
 
 		assert.htmlEqual(target.innerHTML, `
 			main updates: 1
-			child updates: 2
-			child foo: a, rs_foo: 2, foo.length: 1
-			child rs_foo_0: 1
-			child rs_foo_1: 0
-			child rs_foo_2: 0
-			child rs_foo_3: 0
-			child rs_foo_4: 0
 		`);
 
+	
 		// SVELTE 'unmodified': bad -> would trigger main-update +1 (wrong / unwanted) only, nothing else!
 		component.add_item_to_child_foo_at_specific_index(3, 'b');
-		compileOptions.accessorsAsync ? flush() : null;
+		await tick();
 
 		assert.htmlEqual(target.innerHTML, `
 			main updates: 1
-			child updates: 3
-			child foo: a,,,b, rs_foo: 3, foo.length: 4
-			child rs_foo_0: 2
-			child rs_foo_1: 0
-			child rs_foo_2: 0
-			child rs_foo_3: 1
-			child rs_foo_4: 0
 		`);
 
+	
 		// SVELTE 'unmodified': bad -> would trigger main-update +1 (wrong / unwanted) only, nothing else!
 		component.add_item_to_child_foo_at_specific_index(1, {});
-		compileOptions.accessorsAsync ? flush() : null;
+		await tick();
 
 		assert.htmlEqual(target.innerHTML, `
 			main updates: 1
-			child updates: 4
-			child foo: a,[object Object],,b, rs_foo: 4, foo.length: 4
-			child rs_foo_0: 3
-			child rs_foo_1: 1
-			child rs_foo_2: 0
-			child rs_foo_3: 2
-			child rs_foo_4: 0
 		`);
 
+		/*
 		// SVELTE 'unmodified': bad -> would trigger main-update +1 (wrong / unwanted) only, nothing else!
 		component.add_item_to_child_foo_at_specific_index(2, 20);
 		compileOptions.accessorsAsync ? flush() : null;
 
 		assert.htmlEqual(target.innerHTML, `
 			main updates: 1
-			child updates: 5
-			child foo: a,[object Object],20,b, rs_foo: 5, foo.length: 4
-			child rs_foo_0: 4
-			child rs_foo_1: 2
-			child rs_foo_2: 1
-			child rs_foo_3: 3
-			child rs_foo_4: 0
 		`);
 
 		// SVELTE 'unmodified': bad -> would trigger main-update +1 (wrong / unwanted) only, nothing else!
@@ -88,13 +62,6 @@ export default {
 
 		assert.htmlEqual(target.innerHTML, `
 			main updates: 1
-			child updates: 6
-			child foo: a,[object Object],20,b,1,2,3, rs_foo: 6, foo.length: 5
-			child rs_foo_0: 5
-			child rs_foo_1: 3
-			child rs_foo_2: 2
-			child rs_foo_3: 4
-			child rs_foo_4: 1
 		`);
 
 		// SVELTE 'unmodified': bad -> would trigger main-update +1 (wrong / unwanted) only, nothing else!
@@ -103,13 +70,6 @@ export default {
 
 		assert.htmlEqual(target.innerHTML, `
 			main updates: 1
-			child updates: 7
-			child foo: ax,[object Object],20,b,1,2,3, rs_foo: 7, foo.length: 5
-			child rs_foo_0: 6
-			child rs_foo_1: 4
-			child rs_foo_2: 3
-			child rs_foo_3: 5
-			child rs_foo_4: 2
 		`);
 
 		// No value change check
@@ -122,13 +82,6 @@ export default {
 
 		assert.htmlEqual(target.innerHTML, `
 			main updates: 1
-			child updates: 7
-			child foo: ax,[object Object],20,b,1,2,3, rs_foo: 7, foo.length: 5
-			child rs_foo_0: 6
-			child rs_foo_1: 4
-			child rs_foo_2: 3
-			child rs_foo_3: 5
-			child rs_foo_4: 2
 		`);
 
 		// SVELTE 'unmodified': bad -> would trigger nothing (double wrong / double unwanted)!
@@ -137,13 +90,6 @@ export default {
 
 		assert.htmlEqual(target.innerHTML, `
 			main updates: 1
-			child updates: 8
-			child foo: ax,[object Object],20,b,1,2,3, rs_foo: 8, foo.length: 5
-			child rs_foo_0: 7
-			child rs_foo_1: 5
-			child rs_foo_2: 4
-			child rs_foo_3: 6
-			child rs_foo_4: 3
 		`);
 
 		// IMPORTANT  'accmod' remark:
@@ -155,13 +101,6 @@ export default {
 
 		assert.htmlEqual(target.innerHTML, `
 			main updates: 1
-			child updates: 8
-			child foo: ax,[object Object],20,b,1,2,3, rs_foo: 8, foo.length: 5
-			child rs_foo_0: 7
-			child rs_foo_1: 5
-			child rs_foo_2: 4
-			child rs_foo_3: 6
-			child rs_foo_4: 3
 		`);
 
 
@@ -170,14 +109,8 @@ export default {
 
 		assert.htmlEqual(target.innerHTML, `
 			main updates: 1
-			child updates: 9
-			child foo: a,b,c,d,e, rs_foo: 9, foo.length: 5
-			child rs_foo_0: 8
-			child rs_foo_1: 6
-			child rs_foo_2: 5
-			child rs_foo_3: 7
-			child rs_foo_4: 4
 		`);
+		*/
 	}
 
 };
