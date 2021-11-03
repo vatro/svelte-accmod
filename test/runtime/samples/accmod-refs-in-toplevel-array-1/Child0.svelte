30a04d9bcef37da79629573fe271661728666608
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2021-11-03 01:21:04 +0100

    [tests] Polish / reformat / refactor WIP

--- test/runtime/samples/accmod-refs-in-toplevel-array-1/Child0.svelte
+++ test/runtime/samples/accmod-refs-in-toplevel-array-1/Child0.svelte
@@ -1,27 +1,28 @@
 <script>
-	import { afterUpdate, onMount } from 'svelte'
-	import SubChild0 from './SubChild0.svelte'
+	import { afterUpdate, onMount } from 'svelte';
+	import SubChild0 from './SubChild0.svelte';
 
-	export let children = []
-	let subchild0
-	let updated = 0
-	let rs_c = 0
-	let rs_c_0 = 0
+	export let children = [];
 
-	$: children ? rs_c++ : null 
-	$: children[0] ? rs_c_0++ : null 
+	let subchild0;
+	let updated = 0;
+	let rs_c = 0;
+	let rs_c_0 = 0;
 
-	onMount(()=>{
-		children.push(subchild0)
-	})
+	$: children ? rs_c++ : null;
+	$: children[0] ? rs_c_0++ : null;
 
-	afterUpdate(()=>{
-		updated ++;
-	})
-	
+	onMount(() => {
+		// reactive push
+		children[0] = subchild0
+	});
+
+	afterUpdate(() => {
+		updated++;
+	});
 </script>
 
-child0-updated:{updated}
-rs-child0.children:{rs_c}
-rs-child0.children[0]:{rs_c_0}
-<SubChild0 bind:this={subchild0}/>
+child0 updated: {updated}
+rs child0.children: {rs_c}
+rs child0.children[0]: {rs_c_0}
+<SubChild0 bind:this={subchild0} />
