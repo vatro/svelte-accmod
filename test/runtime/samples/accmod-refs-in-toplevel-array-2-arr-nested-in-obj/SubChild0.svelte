1c8d5514e3c4716ceac7c281520588600ec2340a
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2021-11-03 01:21:04 +0100

    [tests] Polish / reformat / refactor WIP

--- test/runtime/samples/accmod-refs-in-toplevel-array-2-arr-nested-in-obj/SubChild0.svelte
+++ test/runtime/samples/accmod-refs-in-toplevel-array-2-arr-nested-in-obj/SubChild0.svelte
@@ -1,32 +1,33 @@
 <script>
-	import { afterUpdate, onMount } from 'svelte'
-	import SubSubChild0 from './SubSubChild0.svelte'
+	import { afterUpdate, onMount } from 'svelte';
+	import SubSubChild0 from './SubSubChild0.svelte';
 
 	export let children = {
-		all:[]
-	}
-	let subsubchild0
-	let updated = 0
-	let rs_c = 0
-	let rs_c_all = 0
-	let rs_c_all_0 = 0
+		all: [],
+	};
 
-	$: children ? rs_c++ : null 
-	$: children.all ? rs_c_all++ : null 
-	$: children.all[0] ? rs_c_all_0++ : null 
+	let subsubchild0;
+	let updated = 0;
+	let rs_c = 0;
+	let rs_c_all = 0;
+	let rs_c_all_0 = 0;
 
-	onMount(()=>{
-		children.all.push(subsubchild0)
-	})
+	$: children ? rs_c++ : null;
+	$: children.all ? rs_c_all++ : null;
+	$: children.all[0] ? rs_c_all_0++ : null;
 
-	afterUpdate(()=>{
-		updated ++;
-	})
-	
+	onMount(() => {
+		// reactive push
+		children.all[0] = subsubchild0;
+	});
+
+	afterUpdate(() => {
+		updated++;
+	});
 </script>
 
-subchild0-updated:{updated}
-rs-subchild0.children:{rs_c}
-rs-subchild0.children.all:{rs_c_all}
-rs-subchild0.children.all_0:{rs_c_all_0}
-<SubSubChild0 bind:this={subsubchild0}/>
+subchild0 updated: {updated}
+rs subchild0.children: {rs_c}
+rs subchild0.children.all: {rs_c_all}
+rs subchild0.children.all[0]: {rs_c_all_0}
+<SubSubChild0 bind:this={subsubchild0} />
--- test/runtime/samples/accmod-refs-in-toplevel-array-2/_config.js
+++ test/runtime/samples/accmod-refs-in-toplevel-array-2/_config.js
@@ -3,16 +3,21 @@ export default {
 	async test({ assert, component, target, flush, compileOptions }) {
 
 		// on mount / after first update.
-		// REMARK: accessors are async! before first update per default.
+		// REMARK:  accessors are async! before first update per default.
+
+		// 'subchild0.children' is being triggered TWICE on mount because:
+		// - 'subchild0.children' is truthy on init (+1 rs)
+		// - we're adding an element to 'subchild0.children' ([0]) onMount (+1 rs)
+
 		assert.htmlEqual(target.innerHTML, `
-			main-updated:1
-			child0-updated:1
-			subchild0-updated:1
-			rs-subchild0.children:1
-			rs-subchild0.children[0]:0
-			subsubchild0.foo:1
-			subsubchild0-updated:1
-			rs-subsubchild0.foo:1
+			main updated: 1
+			child0 updated: 1
+			subchild0 updated: 1
+			rs subchild0.children: 2
+			rs subchild0.children[0]: 1
+			subsubchild0.foo: 1
+			subsubchild0 updated: 1
+			rs subsubchild0.foo: 1
 		`);
 
 
@@ -20,33 +25,47 @@ export default {
 		// - update ONLY the referenced 'SubSubChild0'-component (child0.subchild0.children[0]) -> do NOT update 'main', 'Child0' or 'SubChild0' components!
 		// - don't trigger 'children'-array's reactive statements -> we haven't changed the child0.subchild0.children array!
 		component.set_foo_of_child0_subchild0_children_0(2);
-		compileOptions.accessorsAsync ? flush() : null;
+		compileOptions.accessorsAsync ? flush() :  null;
 
 		assert.htmlEqual(target.innerHTML, `
-			main-updated:1
-			child0-updated:1
-			subchild0-updated:1
-			rs-subchild0.children:1
-			rs-subchild0.children[0]:0
-			subsubchild0.foo:2
-			subsubchild0-updated:2
-			rs-subsubchild0.foo:2
+			main updated: 1
+			child0 updated: 1
+			subchild0 updated: 1
+			rs subchild0.children: 2
+			rs subchild0.children[0]: 1
+			subsubchild0.foo: 2
+			subsubchild0 updated: 2
+			rs subsubchild0.foo: 2
 		`);
 
-		// ... again.
-		// again check: ok
+		// change value again
 		component.set_foo_of_child0_subchild0_children_0(3);
-		compileOptions.accessorsAsync ? flush() : null;
+		compileOptions.accessorsAsync ? flush() :  null;
 
 		assert.htmlEqual(target.innerHTML, `
-			main-updated:1
-			child0-updated:1
-			subchild0-updated:1
-			rs-subchild0.children:1
-			rs-subchild0.children[0]:0
-			subsubchild0.foo:3
-			subsubchild0-updated:3
-			rs-subsubchild0.foo:3
+			main updated: 1
+			child0 updated: 1
+			subchild0 updated: 1
+			rs subchild0.children: 2
+			rs subchild0.children[0]: 1
+			subsubchild0.foo: 3
+			subsubchild0 updated: 3
+			rs subsubchild0.foo: 3
+		`);
+
+		// don't change value (correct: no effect)
+		component.set_foo_of_child0_subchild0_children_0(3);
+		compileOptions.accessorsAsync ? flush() :  null;
+
+		assert.htmlEqual(target.innerHTML, `
+			main updated: 1
+			child0 updated: 1
+			subchild0 updated: 1
+			rs subchild0.children: 2
+			rs subchild0.children[0]: 1
+			subsubchild0.foo: 3
+			subsubchild0 updated: 3
+			rs subsubchild0.foo: 3
 		`);
 	}
 };
