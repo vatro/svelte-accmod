a9d8447dca66b91222cc5d16d4dcb0339b36d5f6
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2021-11-03 01:21:04 +0100

    [tests] Polish / reformat / refactor WIP

--- test/runtime/samples/accmod-refs-in-toplevel-array-0/_config.js
+++ test/runtime/samples/accmod-refs-in-toplevel-array-0/_config.js
@@ -4,13 +4,18 @@ export default {
 
 		// on mount / after first update.
 		// REMARK: accessors are async! before first update per default.
+
+		// 'main.children' is being triggered TWICE on mount because:
+		// - 'main.children' is truthy on init (+1 rs)
+		// - we're adding an element to 'main.children' ([0]) onMount (+1 rs)
+
 		assert.htmlEqual(target.innerHTML, `
-			main-updated:1
-			rs-main.children:1
-			rs-main.children[0]:0
-			child0.foo:1
-			child0-updated:1
-			rs-child0.foo:1
+			main updated: 1
+			rs main.children: 2
+			rs main.children[0]: 1
+			child0.foo: 1
+			child0 updated: 1
+			rs child0.foo: 1
 		`);
 
 
@@ -18,29 +23,41 @@ export default {
 		// - update ONLY the referenced 'Child0'-component (main.children[0]) -> do NOT update 'main'-component! (this is different in unmodified Svelte)
 		// - don't trigger array's reactive statements -> we haven't changed the main.children array! (this is different in unmodified Svelte)
 		component.set_foo_of_children_0(2);
-		compileOptions.accessorsAsync ? flush() : null;
+		compileOptions.accessorsAsync ? flush() :  null;
 
 		assert.htmlEqual(target.innerHTML, `
-			main-updated:1
-			rs-main.children:1
-			rs-main.children[0]:0
-			child0.foo:2
-			child0-updated:2
-			rs-child0.foo:2
+			main updated: 1
+			rs main.children: 2
+			rs main.children[0]: 1
+			child0.foo: 2
+			child0 updated: 2
+			rs child0.foo: 2
 		`);
 
-		// ... again.
-		// again check: ok
+		// change value again
 		component.set_foo_of_children_0(3);
-		compileOptions.accessorsAsync ? flush() : null;
+		compileOptions.accessorsAsync ? flush() :  null;
 
 		assert.htmlEqual(target.innerHTML, `
-			main-updated:1
-			rs-main.children:1
-			rs-main.children[0]:0
-			child0.foo:3
-			child0-updated:3
-			rs-child0.foo:3
+			main updated: 1
+			rs main.children: 2
+			rs main.children[0]: 1
+			child0.foo: 3
+			child0 updated: 3
+			rs child0.foo: 3
+		`);
+
+		// don't change value
+		component.set_foo_of_children_0(3);
+		compileOptions.accessorsAsync ? flush() :  null;
+
+		assert.htmlEqual(target.innerHTML, `
+			main updated: 1
+			rs main.children: 2
+			rs main.children[0]: 1
+			child0.foo: 3
+			child0 updated: 3
+			rs child0.foo: 3
 		`);
 	}
 };
