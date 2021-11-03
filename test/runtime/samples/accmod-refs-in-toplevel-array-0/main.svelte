bc416c31c3b70ec3445cc37b398e3d94ddfa1b16
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2021-11-03 01:21:04 +0100

    [tests] Polish / reformat / refactor WIP

--- test/runtime/samples/accmod-refs-in-toplevel-array-0/main.svelte
+++ test/runtime/samples/accmod-refs-in-toplevel-array-0/main.svelte
@@ -1,32 +1,33 @@
 <script>
-	import { afterUpdate, onMount } from 'svelte'
-	import Child0 from './Child0.svelte'
+	import { afterUpdate, onMount } from 'svelte';
+	import Child0 from './Child0.svelte';
 
-	export let children = []
-	export let bar = 1
-	let child0
-	let updated = 0
-	let rs_c = 0
-	let rs_c_0 = 0
+	export let children = [];
+	export let bar = 1;
+	
+	let child0;
+	let updated = 0;
+	let rs_c = 0;
+	let rs_c_0 = 0;
 
-	$: children ? rs_c++ : null 
-	$: children[0] ? rs_c_0++ : null 
+	$: children ? rs_c++ : null;
+	$: children[0] ? rs_c_0++ : null;
 
-	onMount(()=>{
-		children.push(child0)
-	})
+	onMount(() => {
+		// REACTIVE!
+		children[0] = child0;
+	});
 
-	afterUpdate(()=>{
-		updated ++;
-	})
+	afterUpdate(() => {
+		updated++;
+	});
 
 	export function set_foo_of_children_0(value) {
 		children[0].foo = value;
 	}
-	
 </script>
 
-main-updated:{updated}
-rs-main.children:{rs_c}
-rs-main.children[0]:{rs_c_0}
-<Child0 bind:this={child0}/>
+main updated: {updated}
+rs main.children: {rs_c}
+rs main.children[0]: {rs_c_0}
+<Child0 bind:this={child0} />
