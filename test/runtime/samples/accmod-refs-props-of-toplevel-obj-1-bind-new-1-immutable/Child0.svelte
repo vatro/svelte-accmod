362244ce21cc279f47f98e353aa614f5c326fc26
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2021-11-04 02:45:15 +0100

    [tests] Polish / reformat / refactor / new tests
    All passing.

---
+++ test/runtime/samples/accmod-refs-props-of-toplevel-obj-1-bind-new-1-immutable/Child0.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { afterUpdate } from 'svelte';
+	import SubChild0 from './SubChild0.svelte';
+
+	export let children = {};
+	export let bar = 1;
+
+	let updated = 0;
+	let rs_c = 0;
+	let rs_c_0 = 0;
+
+	// get's triggered twice on init / on mount:
+	// 	1. children is truthy at init
+	// 	2. we change the children object by creating a new prop on bind:this={children['subchild0']}
+	$: children ? rs_c++ : null;
+	// get's triggered once on mount:
+	// 	- children.subchild0 is truthy after prop 'child0' has been created on bind:this={children['subchild0']}
+	$: children.subchild0 ? rs_c_0++ : null;
+
+	afterUpdate(() => {
+		updated++;
+	});
+</script>
+
+child0-updated:{updated}
+rs-child0.children:{rs_c}
+rs-child0.children.subchild0:{rs_c_0}
+<SubChild0 bind:this={children['subchild0']} />
--- test/runtime/samples/accmod-refs-props-of-toplevel-obj-1-ref-inject/Child0.svelte
+++ test/runtime/samples/accmod-refs-props-of-toplevel-obj-1-ref-inject/Child0.svelte
@@ -1,33 +1,32 @@
 <script>
-	import { afterUpdate, onMount } from 'svelte'
-	import SubChild0 from './SubChild0.svelte'
+	import { afterUpdate, onMount } from 'svelte';
+	import SubChild0 from './SubChild0.svelte';
 
-	export let children = {}
-	export let subchild0
+	export let children = {};
+	export let subchild0;
 
-	let updated = 0
-	let rs_c = 0
-	let rs_c_0 = 0
+	let updated = 0;
+	let rs_c = 0;
+	let rs_c_0 = 0;
 
 	// get's triggered twice on init / on mount:
 	// 	1. children is truthy at init
 	// 	2. we change the children object by creating a new prop on bind:this={subchild0}
-	$: children ? rs_c++ : null 
+	$: children ? rs_c++ : null;
 	// get's triggered once on mount:
 	// 	- children.subchild0 is truthy after prop 'child0' has been created on bind:this={subchild0}
-	$: children.subchild0 ? rs_c_0++ : null 
+	$: children.subchild0 ? rs_c_0++ : null;
 
-	onMount(()=>{
-		children.subchild0 = subchild0
-	})
+	onMount(() => {
+		children.subchild0 = subchild0;
+	});
 
-	afterUpdate(()=>{
-		updated ++;
-	})
-	
+	afterUpdate(() => {
+		updated++;
+	});
 </script>
 
 child0-updated:{updated}
 rs-child0.children:{rs_c}
 rs-child0.children.subchild0:{rs_c_0}
-<SubChild0 bind:this={subchild0}/>
+<SubChild0 bind:this={subchild0} />
