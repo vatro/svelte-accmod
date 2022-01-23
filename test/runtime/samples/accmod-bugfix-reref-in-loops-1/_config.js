94645376655cc9282b4288f60110039f3bde467d
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2022-01-23 23:27:24 +0100

    [FIX] Using a reference to an item of an unexported top-level declared array in a member expression inside a loop

---
+++ test/runtime/samples/accmod-bugfix-reref-in-loops-1/_config.js
@@ -0,0 +1,22 @@
+export default {
+
+	async test({ assert, component, target, flush, compileOptions }) {
+
+		// on mount / after first update.
+		// REMARK: accessors are async! before first update per default.
+
+		component.process_array();
+		compileOptions.accessorsAsync ? flush() : null;
+
+		assert.htmlEqual(target.innerHTML, `
+			script_error?: no
+			outputs_queue[0].bar[0]: 1
+			outputs_queue[0].bar[1]: something_0
+			outputs_queue[1].bar[0]: 2
+			outputs_queue[1].bar[1]: something_1
+			outputs_queue[2].bar[0]: 3
+			outputs_queue[2].bar[1]: something_2
+		`);
+		
+	}
+};
