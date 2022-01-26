934a496657b3e6c0c42535bf25e0dd79170ad09e
Vatroslav Vrbanic <v.vrbanic@cream.gmbh>
2022-01-26 04:11:11 +0100

    [tests] Obligatory flush() in new in 3.46.0

--- test/runtime/samples/inline-style-directive-shorthand/_config.js
+++ test/runtime/samples/inline-style-directive-shorthand/_config.js
@@ -3,15 +3,15 @@ export default {
 		<p style="color: red;"></p>
 	`,
 
-	test({ assert, component, target, window, flush, compileOptions }) {
+	test({ assert, component, target, window, flush }) {
 		const p = target.querySelector('p');
 
 		let styles = window.getComputedStyle(p);
 		assert.equal(styles.color, 'red');
 
 		component.color = 'blue';
-		compileOptions.accessorsAsync ? flush() : null;
-		
+		flush();
+
 		assert.htmlEqual(target.innerHTML, '<p style="color: blue;"></p>');
 
 		styles = window.getComputedStyle(p);
